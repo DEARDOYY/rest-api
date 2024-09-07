9caac4ae9a7f16e511d21c66eed231c01219da2e
Pinya Aungkanarungrut <88120284+DEARDOYY@users.noreply.github.com>
2024-09-07 22:31:04 +0700

    update : func create Event

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 
+	"example.com/rest-api/models"
 	"github.com/gin-gonic/gin"
 )
 
@@ -11,10 +12,29 @@ func main() {
 
 	// GET ,POST ,PUT ,PATCH ,DELETE
 	server.GET("/events", getEvents)
+	server.POST("/events", crateEvent)
 
 	server.Run(":8080") // localhost:8080
 }
 
 func getEvents(context *gin.Context) {
-	context.JSON(http.StatusOK, gin.H{"message": "Success!"})
+	events := models.GetAllEvents()
+	// context.JSON(http.StatusOK, gin.H{"message": "Success!"})
+	context.JSON(http.StatusOK, events)
+}
+
+func crateEvent(context *gin.Context) {
+	var event models.Event
+	err := context.ShouldBindJSON(&event)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data."})
+		return
+	}
+
+	event.ID = 1
+	event.UserID = 1
+
+	context.JSON(http.StatusCreated, gin.H{"message": "Event created!", "event": event})
+
 }
