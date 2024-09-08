20c7327fd0b7197d5c6eb0083ec72940f2fda6bb
Pinya Aungkanarungrut <88120284+DEARDOYY@users.noreply.github.com>
2024-09-08 14:59:26 +0700

    update: func test rest api

--- main.go
+++ main.go
@@ -35,6 +35,8 @@ func crateEvent(context *gin.Context) {
 	event.ID = 1
 	event.UserID = 1
 
+	event.Save()
+
 	context.JSON(http.StatusCreated, gin.H{"message": "Event created!", "event": event})
 
 }
