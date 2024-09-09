c188bb60adabaaeade9664ba8462bc76e29919b0
Pinya Aungkanarungrut <88120284+DEARDOYY@users.noreply.github.com>
2024-09-09 23:43:14 +0700

    create: connect DB

--- db/db.go
+++ db/db.go
@@ -1,17 +1,52 @@
 package db
 
-import "database/sql"
+import (
+	"database/sql"
+
+	_ "github.com/mattn/go-sqlite3"
+)
 
 var DB *sql.DB
 
-func initDB() {
+func InitDB() {
 	DB, err := sql.Open("sqlite3", "api.db")
 
 	if err != nil {
-		panic("Could not connect to database. ")
+		panic("Could not connect to database.")
+	}
+
+	err = DB.Ping()
+	if err != nil {
+		panic("Error connecting to the database")
+	}
+
+	if DB == nil {
+		panic("Database is not initialized")
 	}
 
 	// pool connection
 	DB.SetMaxOpenConns(10)
 	DB.SetMaxIdleConns(5)
+
+	createTables()
+
+	defer DB.Close()
+}
+
+func createTables() {
+	createEventsTable := `
+	CREATE TABLE IF NOT EXISTS events (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		name TEXT NOT NULL,
+		description TEXT NOT NULL,
+		localtion TEXT NOT NULL,
+		date_time DATETIME NOT NULL,
+		user_id INTEGER
+	)`
+
+	_, err := DB.Exec(createEventsTable)
+
+	if err != nil {
+		panic("Could not create events table.")
+	}
 }
--- models/event.go
+++ models/event.go
@@ -1,9 +1,13 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"example.com/rest-api/db"
+)
 
 type Event struct {
-	ID          int
+	ID          int64
 	Name        string    `binding: "required"`
 	Description string    `binding: "required"`
 	Location    string    `binding: "required"`
@@ -14,7 +18,27 @@ type Event struct {
 var events = []Event{}
 
 func (e Event) Save() {
-	events = append(events, e)
+	query := `	
+	INSERT INTO events(name, description, localtion, date_time, user_id) 
+	VALUES (?, ?, ?, ?, ?)`
+
+	stmt, err := db.DB.Prepare(query)
+
+	if err != nil {
+		return
+	}
+
+	defer stmt.Close()
+	result, err := stmt.Exec(e.Name, e.Description, e.Location, e.DateTime, e.UserID)
+
+	if err != nil {
+		return
+	}
+
+	id, err := result.LastInsertId()
+	e.ID = id
+
+	// events = append(events, e)
 }
 
 func GetAllEvents() []Event {
