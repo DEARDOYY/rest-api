2283c87ba03f3ecec6cfc4b7978a6168a119b191
Pinya Aungkanarungrut <88120284+DEARDOYY@users.noreply.github.com>
2024-12-12 22:29:25 +0700

    add function cancel register

--- routes/routes.go
+++ routes/routes.go
@@ -18,7 +18,7 @@ func RegusterRoutes(server *gin.Engine) {
 	authenticated.PUT("/events/:id", updateEvent)
 	authenticated.DELETE("/events/:id", deleteEvent)
 	authenticated.POST("events/:id/register", registerForEvent)
-	authenticated.DELETE("events/:id/register")
+	authenticated.DELETE("events/:id/register", cancelRegistration)
 
 	server.POST("/signup", signup)
 	server.POST("login", login)
